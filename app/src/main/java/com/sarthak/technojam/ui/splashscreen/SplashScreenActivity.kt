0c0ee11e3d5374090cb97342210cfb35217a42be
SHabaj-dev <shabajansari84@gmail.com>
2023-02-14 11:24:43 +0530

    Created Splash Screen

--- app/src/main/java/com/sarthak/technojam/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/sarthak/technojam/ui/splashscreen/SplashScreenActivity.kt
@@ -1,9 +1,15 @@
 package com.sarthak.technojam.ui.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.sarthak.technojam.MainActivity
 import com.sarthak.technojam.R
 import com.sarthak.technojam.databinding.ActivitySplashBinding
 
@@ -17,5 +23,15 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         viewModel = ViewModelProvider(this).get(SpashScreenViewModel::class.java)
+
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity :: class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
